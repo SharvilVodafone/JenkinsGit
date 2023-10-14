6d898816630b95d7b1b77dae46ad040d9fc27fc2
SharvilVodafone <125637083+SharvilVodafone@users.noreply.github.com>
2023-10-14 19:13:26 +0530

    Update Test01.java

--- src/main/java/com/selenium/testng/Test01.java
+++ src/main/java/com/selenium/testng/Test01.java
@@ -17,6 +17,7 @@ public class Test01 {
     @Test
     public void openWebsite(){
         System.out.println("Hello Jenkins");
+        try{
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Sharvil\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.navigate().to("https://www.youtube.com");
@@ -24,6 +25,11 @@ public class Test01 {
         driver.findElements(By.xpath("//span[contains(text(),'Sign in')]/ancestor::a[@aria-label='Sign in']")).get(0).click();
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("sharvil.joshi11@gmail.com");
         driver.findElement(By.xpath("//span[text()='Next']/ancestor::button")).click();
+            System.out.println("Hello again Jenkins");
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
     @AfterTest
     public void newWindow(){
