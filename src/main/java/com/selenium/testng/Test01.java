05b71dcbf0489211227d86d9c929ee67c5e81d0f
sharvil <sharvil.joshi16@vit.edu>
2023-10-14 18:42:29 +0530

    first commit

---
+++ src/main/java/com/selenium/testng/Test01.java
@@ -0,0 +1,38 @@
+package com.selenium.testng;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Test01 {
+    WebDriver driver;
+    @BeforeSuite
+   public void setupEnvironment(){
+
+    }
+    @Test
+    public void openWebsite(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Sharvil\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.youtube.com");
+        driver.manage().window().maximize();
+        driver.findElements(By.xpath("//span[contains(text(),'Sign in')]/ancestor::a[@aria-label='Sign in']")).get(0).click();
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("sharvil.joshi11@gmail.com");
+        driver.findElement(By.xpath("//span[text()='Next']/ancestor::button")).click();
+    }
+    @AfterTest
+    public void newWindow(){
+        String url = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.navigate().to("https://www.google.com");
+        driver.switchTo().window(url);
+        driver.navigate().to("https://www.google.com");
+        driver.close();
+        driver.quit();
+    }
+}
+
